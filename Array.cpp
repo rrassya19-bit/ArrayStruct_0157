c72c580079ee10250c9f53b74665d5e6a0614fd3
rrassya19-bit <rrassys19@gmail.com>
2026-04-06 15:14:20 +0700

    Menampilkan semua data yang sudah diinput ke array kamu satu per satu

--- Array.cpp
+++ Array.cpp
@@ -32,4 +32,12 @@ int main() {
     
     cout << "Data index 4 = ";
     cin >> kamu[4];
+    cout << endl;
+
+    // Menampilkan semua data yang sudah diinput ke array kamu satu per satu
+    cout << "Data pertama = " << kamu[0] << endl;
+    cout << "Data kedua = " << kamu[1] << endl;
+    cout << "Data ketiga = " << kamu[2] << endl;
+    cout << "Data keempat = " << kamu[3] << endl;
+    cout << "Data kelima = " << kamu[4] << endl;
 }
