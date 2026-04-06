dbff7e9f9bca1fa8698ecf5144c0cddd700566d1
rrassya19-bit <rrassys19@gmail.com>
2026-04-06 15:27:13 +0700

    Struct Orang

--- struct.cpp
+++ struct.cpp
@@ -1,2 +1,9 @@
 #include <iostream>
 using namespace std;
+
+//Struct Orang
+struct Orang {
+    string nama;     // Menyimpan nama (teks)
+    string alamat;   // Menyimpan alamat (teks)
+    int umur;        // Menyimpan umur (angka bulat)
+};
