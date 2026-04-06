70ca23307cf184539cac7a3f9fcd4349645916fe
rrassya19-bit <rrassys19@gmail.com>
2026-04-06 15:30:23 +0700

    Tampilkan data yang sudah diinput

--- struct.cpp
+++ struct.cpp
@@ -23,4 +23,9 @@ int main() {
     cin >> mhs.umur;          // Masukkan umur ke dalam struct
 
     cout << endl;             // Baris baru untuk tampilan lebih rapi
+
+    // Tampilkan data yang sudah diinput
+    cout << "Nama : " << mhs.nama << endl;
+    cout << "Alamat : " << mhs.alamat << endl;
+    cout << "Umur : " << mhs.umur << endl;
 }
