28da0252b852c4034deefd4f76d2f0b2b3549829
rrassya19-bit <rrassys19@gmail.com>
2026-04-06 15:07:36 +0700

    Deklarasi array tanpa inisialisasi (hanya cadangan)

--- Array.cpp
+++ Array.cpp
@@ -4,4 +4,7 @@ using namespace std;
 int main() {
     // Deklarasi array dengan inisialisasi
     int aku[5] = {10, 20, 30, 40, 50};
+
+    // Deklarasi array tanpa inisialisasi (hanya cadangan)
+    int kamu[5];
 }
